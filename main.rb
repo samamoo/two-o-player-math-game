a31420200f08c3abd21b80bc5501301ce43ad1df
Samantha Fok <samantha.fok@hotmail.com>
2021-01-05 00:43:17 +0000

    Removed comments

--- main.rb
+++ main.rb
@@ -6,10 +6,8 @@ player1 = Player.new("Player 1", 3)
 player2 = Player.new("Player 2", 3)
 game = Game.new(player1, player2)
 
-while player1.score != 0 && player2.score != 0
-#   # while neither player's score is 0
-#   # generate new turn
-#   # player 1 asks question
+while game_not_over
+  player1.score != 0 && player2.score != 0
   question = Question.new
   question.generate_question
   input = gets.chomp.to_i 
@@ -22,15 +20,11 @@ while player1.score != 0 && player2.score != 0
 
 end
 
-  # puts q1.generate_question
-  # puts q1.get_sum
-  # answer = gets.chomp.to_i
-  # if answer == q1.get_sum
-  #   puts "Yes! You are correct!"
-  # elsif answer != q1.get_sum
-  #   puts "Seriously? No!"
-  #   p1Score.lose_life
-  #   # p1Score.current_score 
-  #   # p2Score.current_score
-  #   # puts "P1: #{p1Score.current_score} vs P2: #{p2Score.current_score.to_s}"
-  # end
+
+
+# game = Game.new()
+# game.start
+# # starts game, generates players, runs loop
+
+# game.after_end 
+# # print out statistics
